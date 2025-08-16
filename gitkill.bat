4f4dc8d93917ec0ec977687b36bb949adbcaf254
CARLOS  FILHO <ysneshy@gmail.com>
2025-08-16 10:52:47 -0300

    :white_check_mark: :bug: :rocket: :alien: :robot: UPGRADE OPRESSOR

--- gitkill.bat
+++ gitkill.bat
@@ -15,7 +15,7 @@ if "%confirm%"=="s" (
     
     echo.
     echo Executando git commit...
-    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: vai estabiliza otum"
+    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: UPGRADE OPRESSOR"
     if !errorlevel! neq 0 (
         echo Erro no git commit.
         pause
