1d69252752948034ae0d17f947501afcc81513ad
CARLOS  FILHO <ysneshy@gmail.com>
2025-08-25 10:02:07 -0300

    :white_check_mark: :bug: :rocket: :alien: :robot: MANUTENCA2O DE EMERGENCIA

--- gitkill.bat
+++ gitkill.bat
@@ -15,7 +15,7 @@ if "%confirm%"=="s" (
     
     echo.
     echo Executando git commit...
-    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: ESTILIZACAO DO CODIGO"
+    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: MANUTENCA2O DE EMERGENCIA"
     if !errorlevel! neq 0 (
         echo Erro no git commit.
         pause
